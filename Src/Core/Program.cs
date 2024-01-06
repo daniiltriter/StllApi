00b8146c19942477d67dc028a91ed7d480fc1385
Daniil Gruzdev <daniilg@brokeree.com>
2024-01-06 13:05:00 +0300

    Packages for hosting installed

---
+++ Src/Core/Program.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var webHost = new WebHostBuilder()
+    .UseKestrel()
+    .ConfigureServices(services =>
+    {
+        services.AddOptions();
+    })
+    .UseStartup<Startup>()
+    .UseUrls()
+    .Build();
+    
+webHost.Run();
