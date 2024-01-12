d1b7d853760271f933f259ac3e45ff44783c4877
Daniil Gruzdev <daniilg@brokeree.com>
2024-01-12 15:27:21 +0300

    Forge proj for ef core dependency added.

---
+++ Src/Forge/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stll.Forge.Configurations;
+using Stll.Types;
+
+namespace Stll.Forge;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new UserEntityConfiguration());
+    }
+}
---
+++ Src/Forge/Configurations/UserEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Stll.Types;
+
+namespace Stll.Forge.Configurations;
+
+public class UserEntityConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(m => m.Id);
+        builder.Property(m => m.Name).IsRequired();
+        builder.Property(m => m.Password).IsRequired();
+        builder.Property(m => m.Role).IsRequired();
+    }
+}
